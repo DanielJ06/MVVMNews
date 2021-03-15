7ea924f3986790152acd78987f7c2d732b763ce2
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-03-15 16:02:55 -0300

    Setting up navigation

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ArticleFragment.kt
@@ -1,7 +1,20 @@
 package com.androiddevs.mvvmnewsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.androiddevs.mvvmnewsapp.R
 
-class ArticleFragment : Fragment(R.layout.fragment_article) {
+class ArticleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_article, container, false)
+    }
+
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SavedNewsFragment.kt
@@ -1,7 +1,20 @@
 package com.androiddevs.mvvmnewsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.androiddevs.mvvmnewsapp.R
 
-class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
+class SavedNewsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_saved_news, container, false)
+    }
+
 }
